67d5ee07412917aa60622320bcb88627f18db1b9
Ozodbek Oripjonov <ozod3837898@gmail.com>
2022-03-15 18:23:44 +0500

    ok

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
     siteMetadata: {
         title: 'My First Gatsby Site',
-        siteUrl: `https://www.yourdomain.tld`
+        siteUrl: `https://myfirstgatsbyappmain.gatsbyjs.io`,
     },
     plugins: [
         'gatsby-plugin-image',
-        'gatsby-plugin-sharp'
-    ]
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: `blog`,
+                path: `${__dirname}/blog`,
+            },
+        },
+    ],
 };
